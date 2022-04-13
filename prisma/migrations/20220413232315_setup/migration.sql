5bdafe68dd540f7a2209ef23e6b92efaa7ca7f0f
Thumendes <thumendess@gmail.com>
2022-04-13 20:23:39 -0300

    Change migration

--- prisma/migrations/20220413232315_setup/migration.sql
+++ prisma/migrations/20220413232315_setup/migration.sql
@@ -16,6 +16,3 @@ CREATE TABLE `Todo` (
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- AddForeignKey
-ALTER TABLE `Todo` ADD CONSTRAINT `Todo_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
